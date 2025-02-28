a4ba92477612fb74322afeee8faa79ad187cfed1
singhsoumya0109 <singhsoumya1103@gmail.com>
2025-02-28 12:43:56 +0530

    auth done

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -4,6 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 const url = process.env.UPSTASH_URL;
 //console.log(url);
-const client = new Redis(url);
-await client.set("foo", "bar");
+export const redis = new Redis(url);
+//await client.set("ghh", "bar");
 //47.43
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
