f4e93872bb740ef766e705f5198c72e7f5be8b5b
singhsoumya0109 <singhsoumya1103@gmail.com>
2025-03-05 16:37:09 +0530

    cart routes done

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -13,6 +14,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+
 
 app.listen(PORT, () => {
     console.log("Server is running at port " + PORT);
