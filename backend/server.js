697158745521dbcaa264ae629bef2820087d80aa
singhsoumya0109 <singhsoumya1103@gmail.com>
2025-03-07 01:21:03 +0530

    backend done

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -17,7 +19,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
+//2.23.30
 
 app.listen(PORT, () => {
     console.log("Server is running at port " + PORT);
